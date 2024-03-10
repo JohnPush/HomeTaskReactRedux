6e2b29a4aef55a3aa16244b5406642e09877bfc9
Evgenii Pushkarev <pushinbrazil@gmail.com>
2024-03-10 15:25:00 -0300

    изменение конфигурации для перехода на TS

--- src/components/CardFilm/CardFilm.jsx
+++ src/components/CardFilm/CardFilm.jsx
@@ -1,17 +1,20 @@
 import styles from './CardFilm.module.css';
 import RatingButton from '../RatingButton/RatingButton';
 import FavoriteButton from '../FavoriteButton/FavoriteButton';
+import { Link } from 'react-router-dom';
 
 function CardFilm({ film }) {
 	return (
-		<div key={film.id} className={styles['card-button']}>
-			<RatingButton valueRating={film.valueRating} />
-			<div>
-				<img src={film.posterFilm || ''} alt="poster" />
+		<Link to={`/movie/${film.id}`} className={styles['link']}>
+			<div key={film.id} className={styles['card-button']}>
+				<RatingButton valueRating={film.valueRating} />
+				<div>
+					<img src={film.posterFilm || ''} alt="poster" />
+				</div>
+				<div>{film.nameFilm || ''}</div>
+				<FavoriteButton />
 			</div>
-			<div>{film.nameFilm || ''}</div>
-			<FavoriteButton />
-		</div>
+		</Link>
 	);
 }
 
--- src/pages/PageMovie/PageMovie.jsx
+++ src/pages/PageMovie/PageMovie.jsx
@@ -1,3 +1,6 @@
+import { useParams } from 'react-router';
+
 export function PageMovie() {
-	return <>PageMovie</>;
+	const { id } = useParams();
+	return <>Movie - {id}</>;
 }
