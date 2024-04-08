d53347b10c339e18d05a14965cda41131d83e7ea
Evgenii Pushkarev <pushinbrazil@gmail.com>
2024-04-08 12:42:02 -0300

    востановил спроверку формы входа

--- src/pages/PageLogin/PageLogin.tsx
+++ src/pages/PageLogin/PageLogin.tsx
@@ -1,39 +1,29 @@
 import styles from './PageLogin.module.css';
-import Button from '../../components/Button/Button';
 import Heading from '../../components/Heading/Heading';
-import Input from '../../components/Input/Input';
-import { FormEvent, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../../store/user.slice';
+import Login from '../../components/Login/Login';
+import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { useNavigate  } from 'react-router-dom';
 
 
 
 export function PageLogin() {
-  	const [userName, setUserName] = useState('');
-  	const dispatch = useDispatch();
-
   	const profiles = useSelector((state: RootState) => state.user.profile);
   	const isUserLoggedIn = profiles?.some(profile => profile.isLogined);
   	const navigate = useNavigate();
-	  
-	const handleSubmit = (e: FormEvent) => {
-		e.preventDefault();
-		dispatch(login(userName));
-	};
 
-    	if (isUserLoggedIn) {
-      		navigate('/');
-    	}
+    if (isUserLoggedIn) {
+    	navigate('/');
+    }
 
 
 	return (
 		<div className={styles['pageLogin']} >
             <div className={styles['containerHeading']}>
 				<Heading heading="Вход" />
+				<Login />
 			</div>
-			<form className={styles['login']} onSubmit={handleSubmit}>
+			{/* <form className={styles['login']} onSubmit={handleSubmit}>
 				<div className={styles['containerInputButton']}>
 					<Input
 						placeholder="Ваше имя"
@@ -42,10 +32,11 @@ export function PageLogin() {
 						value={userName}
            		 		onChange={(e) => setUserName(e.target.value)}
 						showIconSearch={false}
-					/>
+
+/>
 					<Button textButton="Войти в профиль" type="submit"/>
 				</div>
-			</form>
+			</form> */}
 		</div>
 	);
 }
